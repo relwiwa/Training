2947ee2d440d55c779997eb58082fd6632ae6ca6
Walter <relw1wa-github@yahoo.de>
2016-06-16 18:12:53 +0200

    Phonecat: Step 2- Add model, view and controller plus testing the Angular way

--- AngularJS/Phonecat/app/app.js
+++ AngularJS/Phonecat/app/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Module
+
+angular.module('phonecatApp', []);
+
+// Controller using controller as-syntax
+
+angular.module('phonecatApp')
+.controller('PhoneListController', [function () {
+	this.greeting = 'Welcome';
+	
+	this.phones = [
+		{
+			name: 'Nexus S',
+			snippet: 'Fast just got faster with Nexus S.'
+		}, {
+			name: 'Motorola XOOM™ with Wi-Fi',
+			snippet: 'The Next, Next Generation tablet.'
+		}, {
+			name: 'MOTOROLA XOOM™',
+			snippet: 'The Next, Next Generation tablet.'
+		}
+	];	
+}]);

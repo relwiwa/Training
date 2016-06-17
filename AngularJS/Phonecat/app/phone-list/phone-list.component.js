9152163a469c6783393f05a59fbaa263d9211bb0
Walter <relw1wa-github@yahoo.de>
2016-06-17 14:19:17 +0200

    Phonecat: Step 7- Add data retrieval via and unit testing of $http service

--- AngularJS/Phonecat/app/phone-list/phone-list.component.js
+++ AngularJS/Phonecat/app/phone-list/phone-list.component.js
@@ -4,24 +4,17 @@
 angular.module('phoneList')
 .component('phoneList', {
 	templateUrl: 'phone-list/phone-list.template.html',
-	controller: function PhoneListController() {
-		this.query = '';
-		this.orderProp = 'age'; // name || age
+	controller: ['$http', function PhoneListController($http) {
+		var self = this;
+		self.query = '';
+		self.orderProp = 'age'; // name || age
 		
-		this.phones = [
-			{
-				name: 'Nexus S',
-				snippet: 'Fast just got faster with Nexus S.',
-				age: 1
-			}, {
-				name: 'Motorola XOOM\u2122 with Wi-Fi',
-				snippet: 'The Next, Next Generation tablet.',
-				age: 2
-			}, {
-				name: 'MOTOROLA XOOM\u2122',
-				snippet: 'The Next, Next Generation tablet.',
-				age: 3
-			}
-		];	
-	}
+		$http.get('phones/phones.json')
+		.then(function(response) {
+			self.phones = response.data.slice(0, 5);
+		}, function(error) {
+			self.phones = [];
+			console.log("error fetching phones");
+		});
+	}]
 });

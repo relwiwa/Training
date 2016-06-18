820e17da95784c9387532d88511ad349d450eea5
Walter <relw1wa-github@yahoo.de>
2016-06-18 13:16:22 +0200

    Phonecat- Step 12: Add event handler, adapt unit test, add e2e test

--- AngularJS/Phonecat/app/phone-detail/phone-detail.component.js
+++ AngularJS/Phonecat/app/phone-detail/phone-detail.component.js
@@ -6,9 +6,14 @@ angular.module('phoneDetail')
 	controller: ['$http', '$routeParams', function PhoneDetailController($http, $routeParams) {
 		var self = this;
 		
+		self.setImage = function setImage(imageUrl) {
+			self.mainImageUrl = imageUrl;
+		};
+		
 		$http.get('phones/' + $routeParams.phoneId + '.json')
 		.then(function(response) {
 			self.phone = response.data;
+			self.setImage(self.phone.images[0]);
 		});
 	}]
 });
--- AngularJS/Phonecat/app/phone-detail/phone-detail.component.spec.js
+++ AngularJS/Phonecat/app/phone-detail/phone-detail.component.spec.js
@@ -7,12 +7,16 @@ describe('phoneDetail', function() {
 	describe('PhoneDetailController', function() {
 		
 		var ctrl, $httpBackend;
+		var xyzPhoneData = {
+			name: 'phone xyz',
+			images: ['image/url1.png', 'image/url2.png']
+		};
 		
 		beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
 	
 			$httpBackend = _$httpBackend_;
 			$httpBackend.expectGET('phones/xyz.json')
-			.respond({name: 'phone xyz'});
+			.respond(xyzPhoneData);
 			$routeParams.phoneId = 'xyz';
 			
 			// ctrl needs to be instantiated after $httpBackend request
@@ -25,7 +29,7 @@ describe('phoneDetail', function() {
 			
 			$httpBackend.flush();
 			
-			expect(ctrl.phone).toEqual({name: 'phone xyz'});
+			expect(ctrl.phone).toEqual(xyzPhoneData);
 			
 		});
 		

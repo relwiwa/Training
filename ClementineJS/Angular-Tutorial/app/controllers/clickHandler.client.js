570a1af2f9728795bdbea01ba70409521fcb1108
Walter <relw1wa-github@yahoo.de>
2016-08-16 12:03:18 +0200

    ClementineJS: Angular Tutorial- Rebuild app using Angular

--- ClementineJS/Angular-Tutorial/app/controllers/clickHandler.client.js
+++ ClementineJS/Angular-Tutorial/app/controllers/clickHandler.client.js
@@ -1,53 +1,36 @@
 'use strict';
 
 (function() {
-  var addButton = document.querySelector('.btn-add');
-  var deleteButton = document.querySelector('.btn-delete');
-  var clickNbr = document.querySelector('#click-nbr');
-  var apiUrl = 'http://localhost:3000/api/clicks';
 
-  function ready (fn) {
-    if (typeof fn !== 'function') {
-      return;
-    }
+angular.module('clementineApp', ['ngResource']);
 
-    if (document.readyState === 'complete') {
-      return fn();
-    }
+angular.module('clementineApp')
+.controller('clickController', ['$resource', function($resource) {
+  var vm = this;
+  var Click = $resource('/api/clicks');
 
-    document.addEventListener('DOMContentLoaded', fn, false);
-  }
-
-  function ajaxRequest (method, url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        callback(xmlhttp.response);
-      }
-    };
-
-    xmlhttp.open(method, url, true);
-    xmlhttp.send();
-  }
-
-  function updateClickCount (data) {
-    var clicksObject = JSON.parse(data);
-    clickNbr.innerHTML = clicksObject.clicks;
-  }
-
-  ready(ajaxRequest('GET', apiUrl, updateClickCount));
-
-  addButton.addEventListener('click', function() {
-    ajaxRequest('POST', apiUrl, function() {
-      ajaxRequest('GET', apiUrl, updateClickCount);
+  this.getClicks = function() {
+    Click.get(function(results) {
+      vm.clicks = results.clicks;
     });
-  });
+  };
 
-  deleteButton.addEventListener('click', function() {
-    ajaxRequest('DELETE', apiUrl, function() {
-      ajaxRequest('GET', apiUrl, updateClickCount);
+  this.getClicks();
+
+  // Increment is done server-sideas POST-request via save-function
+  this.addClick = function() {
+    Click.save(function() {
+      vm.getClicks();
     });
-  });
+  };
+
+  // Reset is done server-side as DELETE-request via remove-function 
+  this.resetClicks = function() {
+    Click.remove(function () {
+      vm.getClicks();
+    });
+  };
+
+}]);
 
 })();

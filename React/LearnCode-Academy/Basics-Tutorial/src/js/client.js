b09a9272610aee8c0a4340a2b564773cb7796017
Walter <relw1wa-github@yahoo.de>
2016-10-25 17:19:49 +0200

    React LearnCode Tutorial: Basics #3- Combine multiple components

--- React/LearnCode-Academy/Basics-Tutorial/src/js/client.js
+++ React/LearnCode-Academy/Basics-Tutorial/src/js/client.js
@@ -1,38 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-
-  // class' constructor method
-  constructor() {
-    super();  // needs to be called before everything else
-    this.mother = "Jane"; // class attribute
-  }
-
-  // class method
-  getVal() {
-    return "Hey there, too!!";
-  }
-
-  // render method
-  render() {
-    // simple component logic can be put inside render method
-    const name = "Jackson";
-    return (
-      <div>
-        <h1>Let's see what we can do</h1>
-        <ul>
-          <li>This is just static text.</li>
-          <li>This is the value of a constant: {name}</li>
-          <li>This is the result of some maths: {5 + 8}</li>
-          <li>This is the return value of some IIFE: {(function() { return "Hey, there!"; })()}</li>
-          <li>This is the return value of a class method: {this.getVal()}</li>
-          <li>This is the value of a class attribute: {this.mother}</li>
-        </ul>
-      </div>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 

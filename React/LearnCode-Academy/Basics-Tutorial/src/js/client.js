fa1fffc3a14771a3241f4deb71817cb471c4baa8
Walter <relw1wa-github@yahoo.de>
2016-10-25 17:18:17 +0200

    React LearnCode Tutorial: Basics #2- Understand basic component rendering

--- React/LearnCode-Academy/Basics-Tutorial/src/js/client.js
+++ React/LearnCode-Academy/Basics-Tutorial/src/js/client.js
@@ -3,9 +3,33 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
 
+  // class' constructor method
+  constructor() {
+    super();  // needs to be called before everything else
+    this.mother = "Jane"; // class attribute
+  }
+
+  // class method
+  getVal() {
+    return "Hey there, too!!";
+  }
+
+  // render method
   render() {
+    // simple component logic can be put inside render method
+    const name = "Jackson";
     return (
-      <h1>It works!</h1>
+      <div>
+        <h1>Let's see what we can do</h1>
+        <ul>
+          <li>This is just static text.</li>
+          <li>This is the value of a constant: {name}</li>
+          <li>This is the result of some maths: {5 + 8}</li>
+          <li>This is the return value of some IIFE: {(function() { return "Hey, there!"; })()}</li>
+          <li>This is the return value of a class method: {this.getVal()}</li>
+          <li>This is the value of a class attribute: {this.mother}</li>
+        </ul>
+      </div>
     );
   }
 }

c63669c10763de3b0415db88c779343ea42b0c54
Walter <relw1wa-github@yahoo.de>
2017-03-21 21:36:56 +0100

    React Tic Tac Toe: Implement game with react

--- React/tic-tac-toe-tutorial/src/components/square.js
+++ React/tic-tac-toe-tutorial/src/components/square.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+const Square = (props) => {
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;

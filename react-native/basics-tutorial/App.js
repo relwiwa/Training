d812a544d42f35bd0ef8bfeb0ee3ddf6617ce1e7
Walter <relw1wa-github@yahoo.de>
2018-06-28 21:05:38 +0100

    React Native Basics: Use fixed and flex dimensions for components

--- react-native/basics-tutorial/App.js
+++ react-native/basics-tutorial/App.js
@@ -3,6 +3,8 @@ import { Image, Text, View } from 'react-native';
 
 import Bananas from './Components/Bananas';
 import Blink from './Components/Blink';
+import FlexDimensions from './Components/FlexDimensions';
+import FixedDimensions from './Components/FixedDimensions';
 import Greeting from './Components/Greeting';
 import HelloWorld from './Components/HelloWorld';
 import LotsOfStyles from './Components/LotsOfStyles';
@@ -10,14 +12,18 @@ import LotsOfStyles from './Components/LotsOfStyles';
 export default class App extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center' }}>
-        <HelloWorld />
-        <Bananas />
-        <Greeting name="Rexxar" />
-        <Greeting name="Jaina" />
-        <Greeting name="Valeera" />
-        <Blink text="Blink Blink" />
-        <LotsOfStyles />
+      <View style={{ flex: 1 }}>
+        <View style={{ alignItems: 'center' }}>
+          <HelloWorld />
+          <Bananas />
+          <Greeting name="Rexxar" />
+          <Greeting name="Jaina" />
+          <Greeting name="Valeera" />
+          <Blink text="Blink Blink" />
+          <LotsOfStyles />
+        </View>
+        <FixedDimensions />
+        <FlexDimensions />
       </View>
     );
   }

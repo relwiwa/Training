e45624991e2ae57f519325b7d8744c14e5d19481
Walter <relw1wa-github@yahoo.de>
2018-06-28 16:13:55 +0100

    React Native Basics: Create component that uses state

--- react-native/basics-tutorial/App.js
+++ react-native/basics-tutorial/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Image, Text, View } from 'react-native';
 
 import Bananas from './Components/Bananas';
+import Blink from './Components/Blink';
 import Greeting from './Components/Greeting';
 import HelloWorld from './Components/HelloWorld';
 
@@ -14,6 +15,7 @@ export default class App extends Component {
         <Greeting name="Rexxar" />
         <Greeting name="Jaina" />
         <Greeting name="Valeera" />
+        <Blink text="Blink Blink" />
       </View>
     );
   }

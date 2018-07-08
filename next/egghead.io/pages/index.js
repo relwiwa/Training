3fe58e14157a482b8f6f89c4f654cf09919c2aa9
Walter <relw1wa-github@yahoo.de>
2018-07-08 17:14:16 +0100

    Next.js Egghead Tutorial: Integrate MaterialUI via HOC and create Header component

--- next/egghead.io/pages/index.js
+++ next/egghead.io/pages/index.js
@@ -1,6 +1,10 @@
+import Header from '../components/Header';
+import withMaterialUI from '../shared/MaterialUI/withMaterialUI';
+
 const Index = ({ title = 'Hello from Next.js' }) =>
 <div>
+  <Header />
   <h2>{title}</h2>
 </div>;
 
-export default Index;
+export default withMaterialUI(Index);
